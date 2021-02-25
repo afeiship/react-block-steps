de4c5f945b8caab991358b29d171c35a88d7c516
afeiship <1290657123@qq.com>
2021-02-25 10:41:55 +0800

    chore: use jswork scope

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import noop from '@feizheng/noop';
-import objectAssign from 'object-assign';
 import { Card } from 'antd';
 
 const CLASS_NAME = 'react-ant-card';
